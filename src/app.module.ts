4d1181b1e4e69173f364c9ad681bbb7fc7bc0192
kardasch404 <zz2406143@gmail.com>
2026-01-22 16:48:43 +0100

    chore(cart): setup redis connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import appConfig from '@shared/config/app.config';
 import databaseConfig from '@shared/config/database.config';
 import redisConfig from '@shared/config/redis.config';
+import { RedisModule } from '@infrastructure/cache/redis/redis.module';
+import { PrismaModule } from '@infrastructure/database/prisma/prisma.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import redisConfig from '@shared/config/redis.config';
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
       load: [appConfig, databaseConfig, redisConfig],
     }),
+    RedisModule,
+    PrismaModule,
   ],
   controllers: [],
   providers: [],
