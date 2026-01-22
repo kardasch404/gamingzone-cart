d39923ce238df4e73f8d4cd870c42539af101332
kardasch404 <zz2406143@gmail.com>
2026-01-22 16:48:04 +0100

    chore(cart): configure dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from '@shared/config/app.config';
+import databaseConfig from '@shared/config/database.config';
+import redisConfig from '@shared/config/redis.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      load: [appConfig, databaseConfig, redisConfig],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
